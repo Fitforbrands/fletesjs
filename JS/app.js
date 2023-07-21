37c15b6a809867e42e15af96968bab652a7bd37c
gabrielcapuia <gabriel@fitforbrands.com>
2023-07-21 02:39:04 -0300

    Local Storage y Toastyfy

--- JS/app.js
+++ JS/app.js
@@ -112,10 +112,14 @@ resultados.addEventListener("click", (e) => {
 
 function VerificaDB() {
   let favo = JSON.parse(localStorage.getItem("favoritos"));
-  if ((empresas.length = favo.length)) {
-    console.log("sin modificaciones de DB");
+  if (favo) {
+    if ((empresas.length = favo.length)) {
+      console.log("sin modificaciones de DB");
+    } else {
+      console.log("Local storage no coincide con DB");
+      localStorage.setItem("favoritos", JSON.stringify(empresas));
+    }
   } else {
-    console.log("Local storage no coincide con DB");
     localStorage.setItem("favoritos", JSON.stringify(empresas));
   }
 }
