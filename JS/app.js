d8b481b4dd6f67eb9a56cb21f39f8ad5d169bb5b
gabrielcapuia <gabriel@fitforbrands.com>
2024-06-06 17:54:23 -0300

    correccion

--- JS/app.js
+++ JS/app.js
@@ -13,7 +13,7 @@ let resultadoSuma = 0;
 // cargo empresas
 
 function cargardatos() {
-  fetch(URLserver)
+  fetch(URL)
     .then((response) => response.json())
     .then((data) => empresas.push(...data))
     .then(() => console.table(empresas))
